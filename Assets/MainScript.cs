dc4b42d8e82870c6e3bb331caf13c3c8e80eb5ef
Martin <martin71211@gmail.com>
2024-06-25 17:19:52 -0300

    Empezado

---
+++ Assets/MainScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public GameObject[] arrayproductos;
+    void Start()
+    {
+        foreach(GameObject producto in arrayproductos)
+        {
+            producto.SetActive(false);
+        }
+        int x = Random.Range(0, arrayproductos.Length - 1);
+        arrayproductos[x].SetActive(true);
+        x = Random.Range(0, arrayproductos.Length - 1);
+        arrayproductos[x].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
